6ae0430f6d2571e2731d95a26cc6c7014d4e3162
David <zemoto@gmail.com>
2024-03-18 15:06:55 -0400

    Use relative path instead of absolute path to save config
    The absolute path was next to the app, and relative paths are relative to the app, so just use the file name instead of the full path.

--- VlcScriptPlayer/VlcScriptPlayer/ConfigSerializer.cs
+++ VlcScriptPlayer/VlcScriptPlayer/ConfigSerializer.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using System.Reflection;
 using System.Text.Json;
 using VlcScriptPlayer.UI;
 
@@ -8,22 +7,21 @@ namespace VlcScriptPlayer;
 internal static class ConfigSerializer
 {
    private const string _configName = "config.json";
-   private static readonly string _configFilePath = Path.Combine( Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ), _configName );
 
    public static MainViewModel ReadFromFile()
    {
-      if ( !File.Exists( _configFilePath ) )
+      if ( !File.Exists( _configName ) )
       {
          return new();
       }
 
-      var configString = File.ReadAllText( _configFilePath );
+      var configString = File.ReadAllText( _configName );
       return JsonSerializer.Deserialize<MainViewModel>( configString );
    }
 
    public static void SaveToFile( MainViewModel mainVm )
    {
       var configJson = JsonSerializer.Serialize( mainVm );
-      File.WriteAllText( _configFilePath, configJson );
+      File.WriteAllText( _configName, configJson );
    }
 }
